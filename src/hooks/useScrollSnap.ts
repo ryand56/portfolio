486cc3bd547e0333bb05f197b8ac2dd47188ac71
Ryan Omasta <ryand@emeraldsys.xyz>
2023-05-30 16:35:15 -0600

    Add missing files

---
+++ src/hooks/useScrollSnap.ts
@@ -0,0 +1,35 @@
+import { useEffect, useState } from "react";
+import { linear } from "../util/easing";
+import createScrollSnap from "scroll-snap";
+
+export interface ScrollSnapSettings {
+    snapDestinationX?: string;
+    snapDestinationY?: string;
+    timeout?: number;
+    duration?: number;
+    threshold?: number;
+    snapStop?: boolean;
+    easing?: (t: number) => number;
+};
+
+const useScrollSnap = (
+    ref: React.MutableRefObject<HTMLElement>,
+    settings?: ScrollSnapSettings,
+    callback?: () => void
+) => {
+    const [scrollBind, setBind] = useState(() => () => {});
+    const [scrollUnbind, setUnbind] = useState(() => () => {});
+
+    useEffect(() => {
+        const element = ref.current;
+
+        if (element) {
+            const { bind, unbind } = createScrollSnap(element, settings, callback);
+            setBind(() => bind);
+            setUnbind(() => unbind);
+        }
+    }, []);
+
+    return [scrollBind, scrollUnbind];
+};
+export default useScrollSnap;

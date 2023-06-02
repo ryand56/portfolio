e6599187a8593ebc56a6b4486c9b923ee2750a5c
Ryan Omasta <ryand@emeraldsys.xyz>
2023-06-01 18:49:24 -0600

    Update down arrow and check for intersection

--- src/components/IntroSection.tsx
+++ src/components/IntroSection.tsx
@@ -1,14 +1,46 @@
+import { useRef, useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import Link from "next/link";
 import { FaAngleDown } from "react-icons/fa";
 
-const IntroSection = () => ( // m-4 mb-14 md:m-14
-    <div className="flex relative min-h-screen h-screen w-screen p-14">
-        <h1 className="absolute bottom-[4rem] font-bold md:leading-tight text-3xl md:text-5xl">
-            Hey, I'm Ryan👋
-        </h1>
-        <Link href="">
-            <FaAngleDown className="absolute relative left-1/2 bottom-0 animate-bounce-slow" size="30" />
-        </Link>
-    </div>
-);
+const IntroSection = () => { // m-4 mb-14 md:m-14
+    const containerRef = useRef<HTMLDivElement | null>(null);
+    const router = useRouter();
+
+    const [isVisible, setIsVisible] = useState<boolean>(true);
+    let observer: IntersectionObserver;
+    const observerCallback: IntersectionObserverCallback = (entries) => {
+        const [entry] = entries;
+        setIsVisible(entry.isIntersecting);
+    };
+    const observerOptions = {
+        root: null,
+        rootMargin: "0px",
+        threshold: 1.0
+    };
+
+    useEffect(() => {
+        observer = new IntersectionObserver(observerCallback);
+        if (containerRef.current) observer.observe(containerRef.current);
+
+        return () => {
+            if (containerRef.current) observer.unobserve(containerRef.current);
+        };
+    }, [containerRef.current, observerOptions]);
+
+    useEffect(() => {
+        if (isVisible) router.push("/");
+    }, [isVisible]);
+
+    return (
+        <div ref={containerRef} className="flex relative min-h-screen h-screen w-screen p-14">
+            <h1 className="absolute bottom-[4rem] font-bold md:leading-tight text-3xl md:text-5xl">
+                Hey, I'm Ryan👋
+            </h1>
+            <Link href="#about" className="absolute w-[30px] h-[30px] bottom-[30px] left-1/2">
+                <FaAngleDown className="animate-bounce-slow" size="30" />
+            </Link>
+        </div>
+    );
+};
 export default IntroSection;

3136774d67909c646b9a2e8dfd9cef0729be5e77
Ryan Omasta <ryand@emeraldsys.xyz>
2023-07-05 03:59:13 -0600

    Set container to show as transparent

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -10,7 +10,7 @@ const Section = (props: SectionProps) => {
 
     return (
         <div id={id} className="flex relative min-h-screen h-screen w-screen justify-center items-center">
-            {withContainer ? <div id={`${id}_container`} className="bg-[#696969]/25 rounded h-[300px] w-[600px]">
+            {withContainer ? <div id={`${id}_container`} className="bg-transparent rounded h-[300px] w-[600px]">
                 <div id={`${id}_container_content`} className="p-8">
                     <div id={`${id}_title`} className="font-bold text-2xl mb-2">{title}</div>
                     <div id={`${id}_content`}>
